3da59ed8c3e13b62dc073586c1dd24b0fab3b7f7
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2021-03-03 20:48:29 +0000

    chore: Push to docker if building a release tag.

--- buildbot.sh
+++ buildbot.sh
@@ -12,5 +12,6 @@ if [[ "$BRANCH" == "master" ]]; then
   COVERALLS_REPO_TOKEN=$GLOWPROM_COVERALLS_REPO_TOKEN coveralls
   semantic-release publish
 fi
-
-#docker_push.sh
+if [[ ${BRANCH:0:1} == "v" ]]; then
+    ./docker_push.sh
+fi
