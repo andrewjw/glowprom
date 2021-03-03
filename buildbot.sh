b1f49e2129991c4d08c487f0ab3f318d37ec1e70
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2021-03-03 21:02:46 +0000

    chore:Use correct branch name for tags.

--- buildbot.sh
+++ buildbot.sh
@@ -8,10 +8,11 @@ pip3 install -r requirements.txt
 ./run_tests.sh
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Building branch $BRANCH"
 if [[ "$BRANCH" == "master" ]]; then
   COVERALLS_REPO_TOKEN=$GLOWPROM_COVERALLS_REPO_TOKEN coveralls
   semantic-release publish
 fi
-if [[ ${BRANCH:0:1} == "v" ]]; then
+if [[ ${BRANCH:0:7} == "heads/v" ]]; then
     ./docker_push.sh
 fi
