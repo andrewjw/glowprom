648a85a5a72a6b068c9d1d5da19500591588c59e
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2023-03-09 21:56:22 +0000

    feat: Support multiple python versions.

--- buildbot.sh
+++ buildbot.sh
@@ -2,10 +2,46 @@
 
 set -e
 
+python3.8 -m venv venv-3.8
+
+. venv-3.8/bin/activate
+
+pip3 install -r requirements.txt
+./run_tests.sh
+./code_style.sh
+
+deactivate
+
+python3.9 -m venv venv-3.9
+
+. venv-3.9/bin/activate
+
 pip3 install -r requirements.txt
 
-./code_style.sh
 ./run_tests.sh
+./code_style.sh
+
+deactivate
+
+python3.10 -m venv venv-3.10
+
+. venv-3.10/bin/activate
+
+pip3 install -r requirements.txt
+
+./run_tests.sh
+./code_style.sh
+
+deactivate
+
+python3.11 -m venv venv-3.11
+
+. venv-3.11/bin/activate
+
+pip3 install -r requirements.txt
+
+./run_tests.sh
+./code_style.sh
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Building branch $BRANCH"
