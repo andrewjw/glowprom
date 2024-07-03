3f4be5ff73a2f440eddb6495b8e3f429491c4356
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-07-03 21:03:57 +0100

    fix: Fix docker build.

--- docker_push.sh
+++ docker_push.sh
@@ -6,10 +6,10 @@ sudo docker login --username andrewjw --password $DOCKER_TOKEN
 
 sudo docker buildx create --use
 
-sudo docker build --platform linux/amd64,linux/arm64 \
-                  --build-arg VERSION=$TAG \
-                  -t andrewjw/glowprom:$TAG \
-                  -t andrewjw/glowprom:latest .
+sudo docker buildx --platform linux/amd64,linux/arm64 \
+                   --build-arg VERSION=$TAG \
+                   -t andrewjw/glowprom:$TAG \
+                   -t andrewjw/glowprom:latest .
 
 sudo docker push andrewjw/glowprom:$TAG
 
