78948dedf0fdb0616a80e6e565f91c5da71dac18
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-07-03 20:57:02 +0100

    feat: Build both amd64 and arm64 images, and push both latest and tag versions.

--- docker_push.sh
+++ docker_push.sh
@@ -4,6 +4,13 @@ set -e
 
 sudo docker login --username andrewjw --password $DOCKER_TOKEN
 
-sudo docker build --build-arg VERSION=$TAG -t andrewjw/glowprom:$TAG .
+sudo docker buildx --use
+
+sudo docker build --platform linux/amd64,linux/arm64 \
+                  --build-arg VERSION=$TAG \
+                  -t andrewjw/glowprom:$TAG \
+                  -t andrewjw/glowprom:latest .
 
 sudo docker push andrewjw/glowprom:$TAG
+
+sudo docker push andrewjw/glowprom:latest
