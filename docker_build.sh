dd271e0194d468a1ff43ed2ee1fa52b06fda4a1a
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-07-04 10:46:43 +0100

    fix: Allow docker builds to be tested on a branch.

--- docker_build.sh
+++ docker_build.sh
@@ -10,4 +10,4 @@ sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 \
                          --build-arg VERSION=$TAG \
                          -t andrewjw/glowprom:$TAG \
                          -t andrewjw/glowprom:latest \
-                         --push .
+                         $1 .
