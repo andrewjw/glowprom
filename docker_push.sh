9dd06b9cf6c92ef2799ced64ed5b4ed1565b29b8
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-07-04 10:31:36 +0100

    feat: Build an arm v7 docker image for Raspberry Pi 3.

--- docker_push.sh
+++ docker_push.sh
@@ -6,7 +6,7 @@ sudo docker login --username andrewjw --password $DOCKER_TOKEN
 
 sudo docker buildx create --use
 
-sudo docker buildx build --platform linux/amd64,linux/arm64 \
+sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 \
                          --build-arg VERSION=$TAG \
                          -t andrewjw/glowprom:$TAG \
                          -t andrewjw/glowprom:latest \
