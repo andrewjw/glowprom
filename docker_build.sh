380a5ffd6671e8317ffde09093e4cefbec19f53b
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-07-07 14:55:56 +0100

    feat: Install a local .tar.gz in docker to allow testing and more reliable releases.

--- docker_build.sh
+++ docker_build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo docker login --username andrewjw --password $DOCKER_TOKEN
+#sudo docker login --username andrewjw --password $DOCKER_TOKEN
 
 sudo docker buildx create --use
 
@@ -10,4 +10,4 @@ sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 \
                          --build-arg VERSION=$TAG \
                          -t andrewjw/glowprom:$TAG \
                          -t andrewjw/glowprom:latest \
-                         --push .
+                         $1 .
