269780a22d417d6ea19f7016e842ddc484ef7089
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2025-05-29 12:57:28 +0000

    feat: Remove support for Python 3.8.
    BREAKING CHANGE: Remove support for Python 3.8.

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     scripts=["bin/glowprom"],
     classifiers=[
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -52,6 +51,6 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=requirements,
 )
