355a7b7f33e5607962a7c42dec724f257d44a80e
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2026-01-16 14:51:17 +0000

    fix: Fix python_requires in setup.py

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     install_requires=requirements,
 )
